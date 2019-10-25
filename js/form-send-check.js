75f9dc83c6d3857043300e17746a32c210e93605
Adonariz <54304511+Adonariz@users.noreply.github.com>
2019-10-25 16:40:27 +0300

    Дополнил проверку формы

--- js/form-send-check.js
+++ js/form-send-check.js
@@ -1,10 +1,17 @@
 var form = document.querySelector('.hotel-form');
 var arrivalDate = form.querySelector('[name=arrival-date]');
 var departureDate = form.querySelector('[name=departure-date]');
+var adultsAmount = form.querySelector('[name=adult-count]');
+var childrenAmount = form.querySelector('[name=child-count]');
 
 form.addEventListener('submit', function (evt) {
-  if (!arrivalDate.value || departureDate.value) {
+  if (!arrivalDate.value || !departureDate.value) {
     evt.preventDefault();
     console.log('Нужно ввести даты приезда и отъезда');
+  } else {
+    if (adultsAmount.value == 0 && childrenAmount.value == 0) {
+      evt.preventDefault();
+      console.log('Нужно ввести количество людей');
+    }
   }
 });
