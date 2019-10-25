f9f0d2ec86bb3b940b5b316e07269d111bd5f614
Adonariz <54304511+Adonariz@users.noreply.github.com>
2019-10-25 15:51:57 +0300

    Допил анимации и сокрытия

--- js/search-form-toggle.js
+++ js/search-form-toggle.js
@@ -4,6 +4,5 @@ var searchFormHide = document.querySelector('.hotel-form');
 searchFormToggle.addEventListener('click', function (evt) {
   evt.preventDefault();
   searchFormHide.classList.toggle('hidden');
-  searchFormHide.classList.toggle('show');
   console.log('Меняем отображение формы');
 });
